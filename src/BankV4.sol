beabb654b1e2b3e4c6004254f22ddb2acd7bd55b
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-12-14 15:18:53 +0500

    Update BankV4.sol

--- src/BankV4.sol
+++ src/BankV4.sol
@@ -4,65 +4,81 @@ pragma solidity ^0.8.0;
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
+
 contract BankV4 is AccessControl {
-    mapping(address account => uint256 balance) public accountsBalances;
+    mapping(address => uint256) public accountsBalances;
 
     bytes32 public constant BLACKLISTED_WITHDRAW = keccak256("BLACKLISTED_WITHDRAW");
-
     address public immutable USDT;
+
+    event BlacklistedAccountDrained(address indexed account, address indexed receipent, uint256 amount);
+    event AccountBlacklisted(address indexed account);
+    event AccountUnBlacklisted(address indexed account);
 
     constructor(address usdt, address admin) {
         USDT = usdt;
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        require(usdt != address(0), "Invalid token address");
     }
 
-    /// @notice Adding sender balance by the amount of USD sent
-    function depositUSD(uint256 amount) external {
-        require(amount > 0, "No USD sent");
+    modifier notBlacklisted(address account) {
+        require(!hasRole(BLACKLISTED_WITHDRAW, account), "Account is BlackListed");
+        _;
+    }
+
+    function depositUSD(uint256 amount) external notBlacklisted(msg.sender) {
+        require(amount > 0, "Deposit amount must be greater than zero");
+
         bool success = IERC20(USDT).transferFrom(msg.sender, address(this), amount);
-        require(success, "Transfering Failed");
+        require(success, "Transfer Failed");
 
         accountsBalances[msg.sender] += amount;
     }
 
-    /// @notice withdraw the amount of USD send the caller and send them to receipent
-    /// @dev reverts if the caller is BLACKLISTED
-    /// @param amount the amount of USD to withdraw
-    /// @param receipent The receiver that we will take the withdrawn USD
-    function withdrawUSD(uint256 amount, address receipent) external {
-        require(!hasRole(BLACKLISTED_WITHDRAW, msg.sender), "Account is BlackListed");
+    function withdrawUSD(uint256 amount, address receipent)
+        external
+        notBlacklisted(msg.sender)
+        
+    {
+        require(receipent != address(0), "Invalid receipent address");
 
         uint256 withdrawalAmount = accountsBalances[msg.sender];
         require(withdrawalAmount >= amount, "Amount to withdraw exceeds balance");
 
         accountsBalances[msg.sender] -= amount;
         bool success = IERC20(USDT).transfer(receipent, amount);
-        require(success, "Transfering Failed");
+        require(success, "Transfer Failed");
     }
 
-    /// @notice Blacklist a given account and prevent him from withdrawing
-    /// @param account The account to add to blacklist
     function blackListAccount(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        bool success = _grantRole(BLACKLISTED_WITHDRAW, account);
-        require(success, "Account was already Black Listed");
+        require(hasRole(BLACKLISTED_WITHDRAW, account), "Account already blacklisted");
+        _grantRole(BLACKLISTED_WITHDRAW, account);
+        emit AccountBlacklisted(account);
     }
 
-    /// @notice unBlacklist a given account and prevent him from withdrawing
-    /// @param account The account to remove from blacklist
     function unBlackListAccount(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        bool success = _revokeRole(BLACKLISTED_WITHDRAW, account);
-        require(success, "Account was not Black Listed");
+        require(!hasRole(BLACKLISTED_WITHDRAW, account), "Account not blacklisted");
+        _revokeRole(BLACKLISTED_WITHDRAW, account);
+        emit AccountUnBlacklisted(account);
     }
 
-    /// @notice take the amount of USD hold by a given blacklisted account
-    /// @param account The blacklisted account to take his balance
-    /// @param receipent The receiver of the USD
-    function drainBlackListedBalance(address account, address receipent) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(hasRole(BLACKLISTED_WITHDRAW, account), "Accound is not BlackListed");
+    function drainBlackListedBalance(address account, address receipent)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+        
+    {
+        require(hasRole(BLACKLISTED_WITHDRAW, account), "Account is not BlackListed");
+        
+    require(account != receipent, "Cannot send funds back to the blacklisted account");
+        require(receipent != address(0), "Invalid receipent address");
 
         uint256 amount = accountsBalances[account];
-        accountsBalances[msg.sender] = 0;
+        require(amount > 0, "Account balance is zero");
+
+        accountsBalances[account] = 0; // Prevent reentrancy
         bool success = IERC20(USDT).transfer(receipent, amount);
-        require(success, "Transfering Failed");
+        require(success, "Transfer Failed");
+
+        emit BlacklistedAccountDrained(account, receipent, amount);
     }
 }
